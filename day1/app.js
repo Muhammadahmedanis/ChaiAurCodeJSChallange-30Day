989ff36dd192b1bc09e8ab36812d0cd0173cc171
ahmedanis45 <ahmedanis4546@gmail.com>
2024-07-22 23:27:01 +0500

    Initial commit

---
+++ day1/app.js
@@ -0,0 +1,35 @@
+// TASK-1
+var a = 5;
+console.log(a);
+
+// TASK-2
+let b = 6;
+console.log(b);
+
+// TASK-3
+const c = true;
+console.log(c);
+
+// TASK-4
+let str = "Hi";
+let num = 12;
+let bool = false;
+let arr = [1, 3, 5];
+let obj = {
+    name: "Ali",
+    rollNo: 89,
+}
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(obj));
+console.log(typeof(num, arr));
+
+// TASK-5
+let d = 10;
+d = 15;
+console.log(d);
+
+// TASK-6
+const e = 10;
+e = 15;
+console.log(e);
