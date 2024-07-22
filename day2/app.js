c957eb512383597f7b266d45f228a7b6c5821085
ahmedanis45 <ahmedanis4546@gmail.com>
2024-07-22 23:55:31 +0500

    completed till day2

---
+++ day2/app.js
@@ -0,0 +1,64 @@
+// TASK-1
+let a = 3+4;
+console.log(a);
+
+// TASK-2
+let b = 4-2;
+console.log(b);
+
+// TASK-3
+let c = 4*2;
+console.log(c);
+
+// TASK-4
+let d = 4/2;
+console.log(d);
+
+// TASK-5
+let e = 9%3;
+console.log(e);
+
+// TASK-6
+let f = 4;
+f += 6;
+console.log(f);
+
+// TASK-7
+let g = 7;
+g -+ 3;
+console.log(g);
+
+// TASK-8
+if(4 > 2){
+    console.log(true);
+}
+
+// TASK-9
+if(4 < 7){
+    console.log(true);
+}
+
+// TASK-10
+if(1 == true){
+    console.log("correct");
+}
+
+// TASK-11
+if(4 == 4 && false == 0){
+    console.log("correct");
+}
+// TASK-12
+if(9 % 3 == 0 || 9 % 3 == 1){
+    console.log("correct");
+}
+
+// TASK-13
+let v = 12
+if(!v){
+    console.log(v);
+}
+
+// TASK-14
+let x = 12;
+let result = x >= 0 ? "Positive num" : "Negative number"; 
+console.log(result);
---
+++ day3/app.js
@@ -0,0 +1,38 @@
+// TASK-1
+let checkNum = -10;
+if(checkNum == 0){
+    console.log('num is:', checkNum);
+}
+else if (checkNum > 0){
+    console.log("num is +ve greater than zero:", checkNum);
+}
+else{
+    console.log("num is -ve:", checkNum);
+}
+
+// TASK-2
+let userAge = 14;
+if(userAge >= 18){
+    console.log("Eligible to vote");
+}
+else {
+    console.log("not eligible to vote");
+}
+
+// TASK-3
+let num1 = 3, num2 = 6, num3 = 9;
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is greater:", num1);
+}
+else if(num2 > num3 && num2 > num1){
+    console.log("num2 is greater:", num2);
+}
+else {
+    console.log("num3 is greater:", num3);
+}
+
+
+// TASK-4
+// TASK-5
+// TASK-6
+// TASK-7
